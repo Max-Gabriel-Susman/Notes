6ed28c375254962b408c7c8d3f1ae75024c5a60a
Max_Gabriel_Susman <gabe.susman@gmail.com>
2020-08-31 19:16:07 -0600

    implemented sql-lite data persistence

--- Notes/App.xaml.cs
+++ Notes/App.xaml.cs
@@ -1,19 +1,51 @@
 ﻿using System;
 using System.IO;
 using Xamarin.Forms;
+using Notes.Data;
 
 namespace Notes
 {
     public partial class App : Application
     {
-        public static string FolderPath { get; private set; }
+        // Singleton
+        static NoteDatabase database;
+
+        public static NoteDatabase Database
+        {
+            // I need to grok the get keyword
+            get
+            {
+                if (database == null)
+                {
+                    // Instantiates the singleton database in the event that it hasn't been instantiated yet
+                    database = new NoteDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
+
 
         public App()
         {
+            // what does this guy do again?
             InitializeComponent();
-            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            // Sets up navigation
             MainPage = new NavigationPage(new NotesPage());
         }
-        // ...
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
     }
 }
---
+++ Notes/Data/NoteDatabase.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+using Notes.Models;
+
+namespace Notes.Data
+{
+    public class NoteDatabase
+    {
+        // database is declared
+        readonly SQLiteAsyncConnection _database;
+
+
+        public NoteDatabase(string dbPath)
+        {
+            // database is instantiated
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Note>().Wait();
+        }
+
+        // CRUD Methods
+
+        // Read All
+        public Task<List<Note>> GetNotesAsync()
+        {
+            return _database.Table<Note>().ToListAsync();
+        }
+
+        // Read
+        public Task<Note> GetNoteAsync(int id)
+        {
+            return _database.Table<Note>()
+                            .Where(i => i.ID == id)
+                            .FirstOrDefaultAsync();
+        }
+
+        // Create
+        public Task<int> SaveNoteAsync(Note note)
+        {
+            if (note.ID != 0)
+            {
+                return _database.UpdateAsync(note);
+            }
+            else
+            {
+                return _database.InsertAsync(note);
+            }
+        }
+
+        // Delete
+        public Task<int> DeleteNoteAsync(Note note)
+        {
+            return _database.DeleteAsync(note);
+        }
+    }
+}
